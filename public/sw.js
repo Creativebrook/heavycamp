01fa8224256f0a4cd43188a177af8b99c8333a9f
Creativebrook <creativebrook@hotmail.com>
2026-09-03 12:31:07 +0100

    Refresh PWA cache for new HeavyCamp icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='heavycamp-shell-v6',SHELL=['/manifest.webmanifest','/icon-192.png','/icon-384.png','/icon-512.png','/icon.svg'];
+const CACHE='heavycamp-shell-v7',SHELL=['/manifest.webmanifest','/icon-192.png','/icon-384.png','/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
